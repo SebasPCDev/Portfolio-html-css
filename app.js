e3a64fb5368b2233c38fec6daf9339a270501fed
SebasPCDev <sebpa.16@gmail.com>
2024-01-26 13:04:37 -0500

    hola

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-/* document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     
     const burgerMenu = document.querySelector('.burger-menu');
     const navLinks = document.querySelector('.nav-links');
@@ -7,20 +7,9 @@
     burgerMenu.addEventListener('click', function() {
         navLinks.classList.toggle('show');
     });
-}); */
+});
 
-let array = [0,1,2,3,4,5,6,7,8,9];
-let e  = array.length;
 
-console.time("for1")
-for (let i = 0; i<array.length; i++){
-}
-console.timeEnd("for1")
-
-console.time("for2")
-for (let i = 0; i<e; i++){
-}
-console.timeEnd("for2")
 
 
 
