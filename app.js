58aa1229380ccf53d1bff53a0db22a2e372c6be1
SebasPCDev <sebpa.16@gmail.com>
2024-01-26 13:04:07 -0500

    no hice cambios

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 
-
-
-document.addEventListener('DOMContentLoaded', function() {
+/* document.addEventListener('DOMContentLoaded', function() {
+    
     const burgerMenu = document.querySelector('.burger-menu');
     const navLinks = document.querySelector('.nav-links');
 
     burgerMenu.addEventListener('click', function() {
         navLinks.classList.toggle('show');
     });
-});
+}); */
+
+let array = [0,1,2,3,4,5,6,7,8,9];
+let e  = array.length;
+
+console.time("for1")
+for (let i = 0; i<array.length; i++){
+}
+console.timeEnd("for1")
+
+console.time("for2")
+for (let i = 0; i<e; i++){
+}
+console.timeEnd("for2")
+
+
+
+
